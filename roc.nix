a12f2919d553ac94290c9175b0a62ac4cf10d388
dumbasaroc <timbishop514@gmail.com>
2024-03-14 11:09:05 -0400

    Commit Thu Mar 14 11:09:05 AM EDT 2024

--- roc.nix
+++ roc.nix
@@ -33,7 +33,11 @@
     zsh-powerlevel10k
     gh
     keepass
-    retroarch
+    (retroarch.override {
+      cores = with libretro; [
+        nestopia
+      ];
+    })
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
