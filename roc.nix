4d2a552b944d5acc8b1153873d311f356bfd427e
dumbasaroc <timbishop514@gmail.com>
2024-03-14 10:53:58 -0400

    Commit Thu Mar 14 10:53:58 AM EDT 2024

--- roc.nix
+++ roc.nix
@@ -33,6 +33,7 @@
     zsh-powerlevel10k
     gh
     keepass
+    retroarch
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
